42a30201922da472b4fdc59db6241748259884a6
Ratshili_MG <giftratshili@gmail.com>
2024-08-05 22:47:25 +0200

    Add files via upload

---
+++ FrameWithNameApp.java
@@ -0,0 +1,13 @@
+
+package framewithnameapp;
+
+import com.gift.www.MyDetailsFrame;
+
+public class FrameWithNameApp {
+
+    public static void main(String[] args) {
+        MyDetailsFrame myFrame;
+        myFrame = new MyDetailsFrame();
+    }
+    
+}
---
+++ MyDetailsFrame.java
@@ -0,0 +1,19 @@
+
+package com.gift.www;
+
+import java.awt.Color;
+import javax.swing.JFrame;
+
+public class MyDetailsFrame extends JFrame{
+
+    public MyDetailsFrame() {
+        setTitle("Masala Gift Ratshili");
+        setSize(200,250);
+        setDefaultLookAndFeelDecorated(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setResizable(true);
+        setBackground(Color.yellow);
+        setVisible(true);
+    }
+    
+}
